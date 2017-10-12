7786ff06083f6a5efa821b8d7b72c534dcf11fdb
Heiko Nickerl <dev@heiko-nickerl.com>
2017-10-13 00:22:59 +0200

    filter now works with regex

--- src/datamodel.py
+++ src/datamodel.py
@@ -1,4 +1,5 @@
 from mylogger import logger
+import re
 
 class DataModel:
     def __init__( self, entries ):
@@ -13,16 +14,19 @@ class DataModel:
         self.filter( "" )
 
     def filter ( self, filter_string ):
-        filter_string = filter_string.lower()
-        if self.filter_string != "" and filter_string != "" and self.filter_string in filter_string:
-            logger.info("optimized. len filtered entries: {}".format(len(self.filtered_entries)))
-            self.filtered_entries[:] = [x for x in self.filtered_entries if filter_string in x.name.lower()]
+        #filter_string = filter_string.lower()
+        #if self.filter_string != "" and filter_string != "" and self.filter_string in filter_string:
+        #    logger.info("optimized. len filtered entries: {}".format(len(self.filtered_entries)))
+        #    self.filtered_entries[:] = [x for x in self.filtered_entries if filter_string in x.name.lower()]
         # rebuild filter list from scratch
-        else:
-            self.filtered_entries.clear()
-            for entry in self.entries:
-                if filter_string in entry.name.lower():
-                    self.filtered_entries.append( entry )
+        if filter_string[-1:] == "\\":
+            return
+        self.filtered_entries.clear()
+        p = re.compile(filter_string, re.IGNORECASE)
+        for entry in self.entries:
+            if p.search(entry.name):
+            #if filter_string in entry.name.lower():
+                self.filtered_entries.append( entry )
         self.filter_string = filter_string
 
     def get_filtered_entries( self ):
