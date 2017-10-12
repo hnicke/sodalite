4573c63a4e9fe403d105822097559becdd75d4b5
Heiko Nickerl <dev@heiko-nickerl.com>
2017-10-13 00:25:15 +0200

    delete obsolete code

--- src/datamodel.py
+++ src/datamodel.py
@@ -14,18 +14,12 @@ class DataModel:
         self.filter( "" )
 
     def filter ( self, filter_string ):
-        #filter_string = filter_string.lower()
-        #if self.filter_string != "" and filter_string != "" and self.filter_string in filter_string:
-        #    logger.info("optimized. len filtered entries: {}".format(len(self.filtered_entries)))
-        #    self.filtered_entries[:] = [x for x in self.filtered_entries if filter_string in x.name.lower()]
-        # rebuild filter list from scratch
         if filter_string[-1:] == "\\":
             return
         self.filtered_entries.clear()
         p = re.compile(filter_string, re.IGNORECASE)
         for entry in self.entries:
             if p.search(entry.name):
-            #if filter_string in entry.name.lower():
                 self.filtered_entries.append( entry )
         self.filter_string = filter_string
 
