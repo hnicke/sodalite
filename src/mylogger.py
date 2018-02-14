5bda03ac9b59018a7604040b3849974b988b59c0
Heiko Nickerl <dev@heiko-nickerl.com>
2018-02-14 11:54:27 +0100

    Make log directory customizable

--- src/mylogger.py
+++ src/mylogger.py
@@ -4,7 +4,7 @@ import os
 
 logger = logging.getLogger('sodalite')
 logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler( os.getenv('SODALITE_LOG'))
+fh = logging.FileHandler( os.getenv('LOG_FILE'))
 fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
