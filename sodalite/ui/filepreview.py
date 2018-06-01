9782d8aee672249d4877205777f0a6de52f3a0c4
Heiko Nickerl <dev@heiko-nickerl.com>
2018-06-01 12:35:44 +0200

    Use generators for improved speed

--- sodalite/ui/filepreview.py
+++ sodalite/ui/filepreview.py
@@ -1,9 +1,13 @@
+import logging
+
 import pygments
 from pygments import lexers
 from pygments.lexers.shell import BashLexer
 from urwid import Text, ListBox, SimpleListWalker
 
 from ui import theme
+
+logger = logging.getLogger(__name__)
 
 
 class FilePreview(ListBox):
@@ -19,12 +23,16 @@ class FilePreview(ListBox):
         if content:
             try:
                 lexer = lexers.guess_lexer_for_filename(self.model.current_entry.path, content, stripnl=False,
-                                                        stripall=False, ensurenl=False, tabsize=0)
+                                                        ensurenl=False)
+                logger.debug('Detected lexer by filename')
             except pygments.util.ClassNotFound:
                 try:
-                    lexer = lexers.guess_lexer(content, stripnl=False, stripall=False, ensurenl=False, tabsize=0)
+                    lexer = lexers.guess_lexer(content, stripnl=False, ensurenl=False)
+                    logger.debug('Detected lexer by file content')
                 except pygments.util.ClassNotFound:
-                    lexer = BashLexer(stripnl=False, stripall=False, ensurenl=False, tabsize=0)
+                    lexer = BashLexer(stripnl=False, ensurenl=False)
+                    logger.debug('Using fallback lexer')
+            logger.info("Viewing file content - using {} for highlighting".format(type(lexer).__name__))
             tokens = list(lexer.get_tokens(self.model.file_content))
             tokens_with_line_numbers = inject_linenumbers(tokens)
             self.body.clear()
@@ -32,20 +40,18 @@ class FilePreview(ListBox):
 
 
 def inject_linenumbers(tokens):
-    splitted = [[]]
+    line = []
     pos = 0
-    splitted[pos].append((theme.line_number, u"{:>2} ".format(pos + 1)))
-    for format, token in tokens:
+    line.append((theme.line_number, u"{:>2} ".format(pos + 1)))
+    for attr, token in tokens:
         while '\n' in token:
             index = token.index('\n')
             begin = token[:index]
             token = token[index + 1:]
             if begin:
-                splitted[pos].append((format, begin))
+                line.append((attr, begin))
             pos += 1
-            splitted.append([])
-            splitted[pos].append((theme.line_number, u'{:>2} '.format(pos + 1)))
+            yield line
+            line = [(theme.line_number, u'{:>2} '.format(pos + 1))]
         if token:
-            splitted[pos].append((format, token))
-    del splitted[pos]
-    return splitted
+            line.append((attr, token))
