f2e876b27f088d29fc1d18b13d16d8f85a683773
Heiko Nickerl <dev@heiko-nickerl.com>
2021-03-06 18:34:41 +0100

    Remove leaky error handling from cd replacement

--- bin/shell-integration.sh
+++ bin/shell-integration.sh
@@ -64,22 +64,7 @@ elif [ $shell = 'bash' ]; then
 fi
 
 if ! [ "$SODALITE_CD_INTERCEPTION" = 'false' ]; then
-    function cd { 
-        last=${@: -1}
-        # catching the errors here, so user does not see an inconvenient error message
-        if [ "$@" ]; then
-            if ! [ -e "$last" ]; then
-                echo "cd: no such file or directory: $last" > /dev/stderr
-                return 1
-            elif ! [ -d "$last" ]; then
-                echo "cd: not a directory: $last" > /dev/stderr
-                return 1
-            elif ! [ -x "$last" ]; then
-                echo "cd: permission denied: $last" > /dev/stderr
-                return 1
-            fi
-        fi
-        [[ "$argv[-1]" =~ ^(\.|\.\.)$ ]] ||
+    function cd {
         (
             nohup sodalite --update-access "$last" &
         ) >/dev/null 2>&1
